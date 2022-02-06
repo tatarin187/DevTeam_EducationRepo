7bb9ab999183d6449ed92d3a235cfbf0aa8e8f72
mansurn <mansurn187@gmail.com>
2022-02-06 19:36:51 +0600

    Реализация задания по пункту №2.
     - Правка замечаний

--- EducationSolution/DbProject/dbo/Tables/User.sql
+++ EducationSolution/DbProject/dbo/Tables/User.sql
@@ -4,7 +4,7 @@
     [LastName]  NVARCHAR (50) NOT NULL,
     [CreatedAt] DATETIME2 (7) NOT NULL,
     [IsDeleted] BIT           CONSTRAINT [DF_User_IsDeleted] DEFAULT ((0)) NOT NULL,
-    [RoleId] INT NOT NULL, 
+    [RoleId] INT NOT NULL DEFAULT 1, 
     CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED ([Id] ASC), 
     CONSTRAINT [FK_User_UserRole] FOREIGN KEY (RoleId) REFERENCES [dbo].[UserRole]([Id])
 );
